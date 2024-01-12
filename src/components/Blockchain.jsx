fc440fa7f0d7887f51f5f542cce6f07a7a492d7b
Bayu Pratama <bayu_gibeon@outlook.com>
2024-01-12 11:51:02 +0700

    add recursion for getNFT

--- src/components/Blockchain.jsx
+++ src/components/Blockchain.jsx
@@ -1,32 +1,90 @@
 import { _debug } from "../Functions";
-import React, { useEffect, useState }  from 'react';
+import React, { useEffect, useState, Children }  from 'react';
 import {GetMetaNet, GetMainNet, GetAccount} from './Context';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import {ChainsData} from './../lib/ChainsData';
+
 
 const Blockchain = () => {
+
+  const contentPerPage = 10;
+
+  const rateLimit = 150;
+  const endPointLimit = 5;
+  let allowedRequests = rateLimit / endPointLimit;
+  let cursor = null;
 
   let metaNet = GetMetaNet();
   let account = GetAccount();
 
   const [collectionState, setCollectionState] = useState();
   const [accountState, setAccountState] = useState(account.current);
+  const [accountChangedState, setAccountChangedState] = useState(account.current);
   const [apiResult, setApiResult] = useState(null);
+  const [chainState, setChainState] = useState();
+  const [totalPageState, setTotalPageState] = useState(0);
+  const [cursorState, setCursorState] = useState([]);
+  const [currentPageState, setCurrentPageState] = useState("");
+  const [fetchState, setFetchState] = useState(false);
 
-  const chainHex = '0x' + account.chainId.toString(16);
-
-  const findAccount = () => {
+  const findAccount = (flag) => {
     const options = {
       method: 'GET',
       headers: { 'Accept': 'application/json', 'X-API-Key': process.env.REACT_APP_MORALIS_API },
     };
 
-    if (accountState !== '' && accountState !== null)
+    setFetchState(false);
+    if (accountState !== '' && accountState !== null && chainState !== null)
     {
-      const apiUrl = 'https://deep-index.moralis.io/api/v2.2/'+accountState+'/nft?chain=' +chainHex;
-      _debug("FETCH MORALIS", apiUrl);
+      let cursorParam = "";
+
+      if (cursorState.length === 0){
+        setCursorState([]);
+        setAccountChangedState(accountState);
+
+      }
+      switch (flag) {
+        case 0:
+            cursorParam = '&cursor=' + "";          
+          break;
+        case -1:
+          if (currentPageState === 2) {
+            cursorParam = '&cursor=' + "";
+          } else {
+            cursorParam = '&cursor=' + cursorState[currentPageState - 2];
+          }
+          break;
+        case 1:
+          cursorParam = '&cursor=' + cursorState[currentPageState - 1];
+          break;
+        case -99:
+          setFetchState(true);
+          break;
+        default:
+          break;
+      }
+
+      const chainHex = '0x' + chainState.toString(16);
+      let apiUrl = 'https://deep-index.moralis.io/api/v2.2/'+accountState+'/nft?chain=' +chainHex
+        + '&limit=' + contentPerPage.toString() + cursorParam
+
       fetch(apiUrl, options)
-      .then((response) => response.json())
-      .then((data) => {
-        setApiResult(data);
+        .then((response) => response.json())
+        .then((data) => {
+          
+          if (data.page > cursorState.length) {
+            setCursorState(old => [...old, data.cursor]);            
+          }
+
+          setCurrentPageState(data.page);
+
+          if (flag ==0) {
+            setApiResult(data);
+          }
       })
       .catch((err) => {
           _debug('fetch error',err.message);
@@ -34,26 +92,84 @@ const Blockchain = () => {
 
     }  
   }
+
+  const ComboLayout = () => {
+    let menuItems = [];
+
+    ChainsData.forEach((data, i) => {
+      menuItems.push(
+        <MenuItem value={data.Id}>{data.Name + " (" + data.Id +")"}</MenuItem>
+      );
+    });
+
+    return (
+      <FormControl fullWidth>
+      <InputLabel id="select-chain">Chain</InputLabel>
+      <Select
+        id="select-chain"
+        value={chainState}
+        label="Chain"
+        onChange={e => setChainState(e.target.value)}
+      >
+        {menuItems}
+      </Select>
+    </FormControl>          
+
+    );
+    
+  }
+
     return (
         <div>
         <div class="input-group mb-3 w050 ">
           <div class="input-group-prepend">
             <span class="input-group-text" id="basic-addon1">Account</span>
           </div>
-          <input type="text" value={accountState} className="form-control w-25" aria-label="account" aria-describedby="basic-addon1" 
-            name="inputAccount" onChange={e => setAccountState(e.target.value)}
-          />
-          <button type="button" className="btn btn-primary" onClick={() => findAccount()}>
-            Find
+          <div className="w-100">
+            <div style={{width: '600px', height: '50px'}}>
+              <input type="text" value={accountState} className="form-control" aria-label="account" aria-describedby="basic-addon1" 
+                name="inputAccount" onChange={e => setAccountState(e.target.value)}
+              />
+            </div>
+          </div>
+          <div style={{width: '300px', height: '50px'}}>
+            <ComboLayout />
+          </div>
+          <button type="button" className="btn btn-primary mx-3" onClick={() => findAccount(0)}>
+              Show All
+          </button>
+          <button type="button" className="btn btn-primary mx-3" onClick={() => findAccount(-99)}>
+              Fetch
           </button>
         </div>
         {(apiResult !== null && apiResult.result.length > 0) ? 
         <>
-          {_debug('apiResult',apiResult)};
           <h4>Found {apiResult.result.length} NFTs</h4>
           <ViewNFTMetadata data={apiResult.result}/>
+          <div className="mt-1 text-center">
+            <div className="mb-3">
+              <h4>Page : {currentPageState}</h4>
+              
+            </div>
+            {(currentPageState === 1) ? <></> :
+              <button type="button" className="btn btn-primary mx-3" onClick={() => findAccount(-1)}>
+                Prev
+              </button>
+            }  
+            {
+              (parseInt(apiResult.result.length) < parseInt(contentPerPage)) ? <></> : 
+              <button type="button" className="btn btn-primary mx-3" onClick={() => findAccount(1)}>
+                Next
+              </button>
+            }          
+
+        </div>
+
         </>
         : <></>}
+
+     
+        
       </div>
 
     );
@@ -69,7 +185,6 @@ function ViewNFTMetadata(data) {
     let keyNames = Object.keys(dataArray[nfts]);
     let columnResult = [];
     let nftData = Object.values(dataArray[nfts]);
-    _debug('dataArray',dataArray[nfts]);
     for (var i=0;i < keyNames.length;i++)
      {
       if (nftData[i] === null) {
@@ -78,43 +193,15 @@ function ViewNFTMetadata(data) {
   
       }
       if (keyNames[i] === 'metadata') {
-        let metaResult = [];
-        if (nftData[i] !== null){
-          let metaObj = JSON.parse(nftData[i]);
-          let metaKeys = Object.keys(metaObj);
-          let metaValues = Object.values(metaObj);
-          for (var m=0; m < metaValues.length; m++ )
-          {          
-            metaResult.push(
-              <>
-              <div className="col col-5">
-                  <div className="card-text">
-                    {metaKeys[m]}
-                  </div>
-              </div>
-              <div className="col col-7">
-                  <div className="card-text">
-                    {metaKeys[m].includes('image') || metaKeys[m].includes('pic') || metaKeys[m].includes('img') ? 
-                      <img className="" src={metaValues[m]} alt="Card image cap"/>
-                      : 
-                      metaValues[m]
-                    }
-                  </div>
-              </div>
-            </>
-            );
-          }
-        }
-
         columnResult.push(
           <div className="card">
             <div className="card-title">
               {keyNames[i]}
             </div>
             <div className="card-body">
-              <div className="row">
-                {metaResult}
-              </div>
+                  {nftData[i] !== null ? 
+                    <ComponentTree data={(nftData[i])} />
+                  : <></>}
             </div>
         </div>
         );
@@ -136,7 +223,6 @@ function ViewNFTMetadata(data) {
       }
      }
 
-     _debug('columnResult',columnResult);
      htmlResult.push(
       <div className="col col-sm-12 col-md-6 mb-5">
           <div className="card">
@@ -159,3 +245,55 @@ function ViewNFTMetadata(data) {
     </div>);
 }
 
+function ComponentTree({data}) {
+  
+  let metaObj = [];
+
+  if (data.constructor.name === "String") //first layer of input is String, further layer become Object because JSON parsing
+  {
+    metaObj = JSON.parse(data);
+  }
+  else {
+    metaObj = data;
+  }
+
+  let metaKeys = Object.keys(metaObj);
+  let metaValues = Object.values(metaObj);
+
+  return(
+    <>
+    <div className="row">
+      {
+        metaValues.map((value, index) => {
+          return (
+            <>
+              <div className="col col-4 py-1">
+                  <div className="card-text">
+                    {metaKeys[index]}
+                  </div>
+              </div>
+              <div className="col col-8 py-1">
+              { 
+                value.constructor.name === "Array" || value.constructor.name === "Object"? 
+                <>
+                  <ComponentTree data={value}/>
+                </> : 
+                <>
+                      <div className="card-text">
+                        {
+                          (metaKeys[index].includes('image') || metaKeys[index].includes('pic') || metaKeys[index].includes('img')) ? 
+                          <img className="nft-image" src={value} alt="Card image cap"/>
+                          : 
+                          value
+                        }
+                      </div>
+                  </>
+                }
+                </div>
+            </>);
+        })
+        }
+        </div>
+    </>
+  );
+}
---
+++ src/lib/ChainsData.js
@@ -0,0 +1,19 @@
+
+export const ChainsData = [
+{
+    Name : 'Polygon Mumbai',
+    Id : 80001
+},
+{
+    Name : 'Polygon Mainnet',
+    Id : 137
+},
+{
+    Name : 'Ethereum Mainnet',
+    Id : 1
+},
+{
+    Name : 'Ethereum Görli',
+    Id : 5
+}
+];
